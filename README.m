04182a09c844300f56245f923ffab65f031f8765
QuincyHoward <50436498+QuincyHoward@users.noreply.github.com>
2024-07-20 21:04:20 +0800

    Add files via upload

---
+++ README.m
@@ -0,0 +1,13 @@
+%% 添加文件夹路径
+addpath(genpath('.'));
+
+%% 打开说明书
+winopen .\DOC\ABCD激光谐振腔仿真.pdf
+
+
+%% 版本信息
+% 作者:                Quincy Howard
+% 联系方式：           quincy.hd@qq.com
+% 文件信息更新平台为   matlab云文件、GitHub、CSDN博客、知乎
+% 若使用请注明来源
+% 最后编辑于           2024 年 07 月 10 日
