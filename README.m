4466e701dbb2dea995127394ee14ed71cba07efd
QuincyHoward <50436498+QuincyHoward@users.noreply.github.com>
2024-07-21 08:07:57 +0800

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,18 @@
 %% 添加文件夹路径
 addpath(genpath('.'));
 
+%% 修改fplot函数
+matlabrootpath=matlabroot;
+path0='\toolbox\matlab\graphics\function\fplot.m';
+filepathname=strcat(matlabrootpath,path0);
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 自行在fplot主函数末尾添加（不是文件末尾）
+% grid on;hold on;
+data ='grid on;hold on;';
+edit(filepathname);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 %% 打开说明书
 winopen .\DOC\ABCD激光谐振腔仿真.pdf
 
